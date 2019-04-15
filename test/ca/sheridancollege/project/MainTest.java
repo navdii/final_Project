05db671c829edf9e06205760659dd7cb7cb7e97f
Deep-Navadiya-Sheridan <navadiyd@ssheridancollege.ca>
2019-04-15 19:02:11 -0400

    Implemented OOD Principal. Refactored in MVC

--- test/ca/sheridancollege/project/MainTest.java
+++ test/ca/sheridancollege/project/MainTest.java
@@ -6,6 +6,8 @@ package ca.sheridancollege.project;
  * and open the template in the editor.
  */
 
+import static ca.sheridancollege.project.Controller.GamePlay.getRankInput;
+import static ca.sheridancollege.project.Controller.GamePlay.getSuitInput;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -46,42 +48,42 @@ public class MainTest {
     @Test
     public void testCheckSuitGood() {
         String suit = "Hearts";
-        boolean result = ca.sheridancollege.project.Main.getSuitInput(suit);
+        boolean result = getSuitInput(suit);
         assertEquals(true, result);
        
     }
     @Test
     public void testCheckSuitBad() {
         String suit = "r";
-        boolean result = ca.sheridancollege.project.Main.getSuitInput(suit);
+        boolean result = getSuitInput(suit);
         assertEquals(false, result);
        
     }
         @Test
     public void testCheckSuitBoundry() {
         String suit = "d";
-        boolean result = ca.sheridancollege.project.Main.getSuitInput(suit);
+        boolean result = getSuitInput(suit);
         assertEquals(true, result);
        
     }
     @Test
     public void testCheckRankGood() {
         int rank =12;
-        boolean result = ca.sheridancollege.project.Main.getRankInput(rank);
+        boolean result = getRankInput(rank);
         assertEquals(true, result);
        
     }
     @Test
     public void testCheckRankBad() {
         int rank= 25;
-        boolean result = ca.sheridancollege.project.Main.getRankInput(rank);
+        boolean result = getRankInput(rank);
         assertEquals(false, result);
        
     }
         @Test
     public void testCheckRankBoundry() {
         int rank= 13;
-        boolean result = ca.sheridancollege.project.Main.getRankInput(rank);
+        boolean result = getRankInput(rank);
         assertEquals(true, result);
        
     }
